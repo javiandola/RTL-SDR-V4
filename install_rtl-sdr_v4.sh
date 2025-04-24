ec1217dec88253dcc1243445cd958d6d7e3e3494
javiandola <96593664+javiandola@users.noreply.github.com>
2025-04-24 21:44:56 +0200

    Add files via upload

---
+++ install_rtl-sdr_v4.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#Linux (Debian)
+#https://www.rtl-sdr.com/V4/
+
+# Colores
+verde="\e[0;32m\033[1m"
+rojo="\e[0;31m\033[1m"
+azul="\e[0;34m\033[1m"
+amarillo="\e[0;33m\033[1m"
+morado="\e[0;35m\033[1m"
+turquesa="\e[0;36m\033[1m"
+gris="\e[0;37m\033[1m"
+fin="\033[0m\e[0m"
+
+#Purge the previous driver:
+
+sudo apt purge ^librtlsdr
+sudo rm -rvf /usr/lib/librtlsdr* /usr/include/rtl-sdr* /usr/local/lib/librtlsdr* /usr/local/include/rtl-sdr* /usr/local/include/rtl_* /usr/local/bin/rtl_*
+
+#Install the RTL-SDR Blog drivers:
+
+sudo apt-get install libusb-1.0-0-dev git cmake pkg-config
+git clone https://github.com/rtlsdrblog/rtl-sdr-blog
+cd rtl-sdr-blog
+mkdir build
+cd build
+cmake ../ -DINSTALL_UDEV_RULES=ON
+make
+sudo make install
+sudo cp ../rtl-sdr.rules /etc/udev/rules.d/
+sudo ldconfig
+
+#Blacklist the DVB-T TV drivers.
+
+echo 'blacklist dvb_usb_rtl28xxu' | sudo tee --append /etc/modprobe.d/blacklist-dvb_usb_rtl28xxu.conf
+
+#Software
+
+sudo apt install -y gqrx-sdr qsstv gpredict
+
+#Reboot
+
+echo -e "${verde}reboot please${fin}"
+exit
